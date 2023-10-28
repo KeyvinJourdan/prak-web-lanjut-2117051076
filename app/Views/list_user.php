01657a2b7cc5c2afe90f94a38ff1edb941ac65fd
KeyvinJourdan <keyvinjourdan06@gmail.com>
2023-10-28 19:28:04 +0700

    commit update-delete

--- app/Views/list_user.php
+++ app/Views/list_user.php
@@ -1,11 +1,11 @@
 <?= $this->extend('layouts/app') ?>
-<?= $this->section('content') ?>
 
+<?= $this->section('content') ?>
 <div class="container">
   <div class="row">
-    <h3 class="mt-5">Tabel User</h3>
-    <a href="<?= base_url('user/create')?>" class="btn btn-primary col-2">Tambah Data</a>
-    <table class="table table-striped py-2">
+    <h3 class="mt-5">DATA MAHASISWA</h3>
+    <a class="btn btn-success ml-auto mb-3" href="<?= base_url('user/create') ?>" method="POST" enctype="multipart/form-data">Tambah Mahasiswa</a>
+    <table class="table table-striped">
       <thead>
         <tr>
           <td>No</td>
@@ -18,17 +18,26 @@
       <tbody>
         <?php
         $no = 1;
-        foreach ($users as $user) { ?>
+        foreach ($users as $user) { ?>  
           <tr>
             <td><?= $no++ ?></td>
             <td><?= $user['nama'] ?></td>
             <td><?= $user['npm'] ?></td>
             <td><?= $user['nama_kelas'] ?></td>
             <td>
-              <a class="btn btn-primary" href="<?= base_url('user/' . $user['id']) ?>">Detail</a>
-              <a class="btn btn-warning" href="">Edit</a>
-              <a class="btn btn-danger" href="">Hapus</a>
-
+              <div class="center-icon d-flex justify-content-center align-items-center">
+              <a class="btn btn-primary" href="<?= base_url('user/' . $user['id'])?>">Detail</a>
+              <a class="btn btn-warning" href="<?= base_url('user/' . $user['id'] . '/edit')?>">Edit</a>
+              <form action="<?= base_url('user/' . $user['id']) ?>" method="POST">
+                <input type="hidden" name="_method" value="DELETE">
+                <?= csrf_field() ?>
+                <button type="submit" value="Delete" class="btn btn-danger">Delete</button>
+              </form>
+              </div>
+            </td>
+            <td>
+            
+              
             </td>
           </tr>
         <?php }
@@ -37,4 +46,4 @@
     </table>
   </div>
 </div>
-<?= $this->endSection('content') ?>
+<?= $this->endSection() ?>
